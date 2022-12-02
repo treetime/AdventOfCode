15093246cdd5203f7d33077c7d457bffaf42b9ce
AJ Ulyate <aj.ulyate@gmail.com>
2022-12-02 14:28:00 +0200

    day 1 part 2

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -5,13 +5,21 @@ import scala.util.chaining._
 
 object Day1 extends App {
 
-  Source.fromFile("src/main/resources/day1")
-    .getLines()
-    .toVector
-    .foldLeft(Seq(Seq.empty[Int])) {
-      case (acc, elem) if elem.isBlank => Seq.empty +: acc
-      case (acc, elem) => acc.headOption.map(l => (l :+ elem.toInt) +: acc.tail).getOrElse(acc)
-    }.map(_.sum).max pipe println
+  topN(1) pipe println
+  topN(3) pipe println
+
+  def topN(n: Int): Int =
+    Source.fromFile("src/main/resources/day1")
+      .getLines()
+      .toVector
+      .foldLeft(Seq(Seq.empty[Int])) {
+        case (acc, elem) if elem.isBlank => Seq.empty +: acc
+        case (acc, elem) => acc.headOption.map(l => (l :+ elem.toInt) +: acc.tail).getOrElse(acc)
+      }
+      .map(_.sum)
+      .sorted
+      .takeRight(n)
+      .sum
 
 
 }
